aee91ee382046951b5a768f66b43757f7d8a93d7
Your Name <you@example.com>
2025-05-20 22:13:11 +0400

    fix errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Collection from './pages/Collection';
 import Detail from './pages/Detail';
 import Company from './pages/Company';
